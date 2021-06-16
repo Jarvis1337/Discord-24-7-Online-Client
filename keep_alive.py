5699dc3aecde2706f2b9307df2cf79b90a3ab9d2
Jack ff <77638593+Jack-ff-31@users.noreply.github.com>
2021-06-16 11:31:02 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -5,7 +5,7 @@ app = Flask('')
 
 @app.route('/')
 def main():
-    return "Subscribe to Mr Furious https://youtube.com/c/MrFurious9"
+    return "https://github.com/Jack-ff-31"
 
 def run():
     app.run(host="0.0.0.0", port=8080)
