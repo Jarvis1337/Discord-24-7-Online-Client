93cc64a71a30f8c40bf2127f1ce88e7de056a74a
Your's Jarvis <77638593+Yours-Jarvis@users.noreply.github.com>
2022-06-12 14:23:56 +0530

    ✔ Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -5,7 +5,7 @@ app = Flask('')
 
 @app.route('/')
 def main():
-    return '<meta http-equiv="refresh" content="0; URL=https://yours-jarvis.github.io/Yours-Jarvis"/>'
+    return '<meta http-equiv="refresh" content="0; URL=https://yours-jarvis.netlify.app/"/>'
 
 def run():
     app.run(host="0.0.0.0", port=8080)
